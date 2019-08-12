6b17d56986a5982e033c1986f547fdb484ca29fa
Krzysztof Krysiak <krysiak86@gmail.com>
2019-08-12 22:22:17 +0200

    WebApi tests

--- FastShotter.API/Controllers/ValuesController.cs
+++ FastShotter.API/Controllers/ValuesController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,23 +14,49 @@ namespace FastShotter.API.Controllers
     {
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public async Task<ActionResult<IEnumerable<string>>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return await Task.Run(() => new string[] { "value1", "value2" });
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public async Task<ActionResult<string>> Get(int id)
         {
-            return "value";
+            return await Task.Run(() => $"value {id}");
+        }
+
+        [HttpGet("newapi/{id:int}")]
+        public ActionResult<string> Get2(int id)
+        {
+            return "hahaha";
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<IActionResult> Post([FromBody] SomeClassValue value)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            value.Value2 = "New Example Value #2";
+            return Created("aaa", value);
         }
+        
+        // POST api/values
+//        [HttpPost]
+//        public async Task<IActionResult> Post([FromBody] SomeClassValue value)
+//        {
+//            if (!ModelState.IsValid)
+//            {
+//                return await Task.Run(() => BadRequest());
+//            }
+//
+//            value.Value2 = "New Example Value #2";
+//            return await Task.Run(() => Created("aaa", value));
+//        }
 
         // PUT api/values/5
         [HttpPut("{id}")]
@@ -41,5 +69,12 @@ namespace FastShotter.API.Controllers
         public void Delete(int id)
         {
         }
+    }
+
+    public class SomeClassValue
+    {
+        public int Id { get; set; }
+        public string Value { get; set; }
+        public string Value2 { get; set; }
     }
 }
---
+++ FastShotter.API/TodoDbContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace FastShotter.API
+{
+    public class TodoDbContext : DbContext
+    {
+        
+    }
+}
