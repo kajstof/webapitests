44c4bfa462d4b0b3235254cd01552921c3b0bd4c
Krzysztof Krysiak <krysiak86@gmail.com>
2019-08-13 20:37:00 +0200

    Add sample unit test

--- FastShotter.Tests/UnitTest1.cs
+++ FastShotter.Tests/UnitTest1.cs
@@ -8,7 +8,19 @@ namespace FastShotter.Tests
         [Fact]
         public void Test1()
         {
+            // Arrange
+            int i = 10;
+            
+            // Act
+            int result = Execute(5);
 
+            // Assert
+            Assert.True(result == 10, "result is true");
+        }
+
+        private int Execute(int i)
+        {
+            return i * 2;
         }
     }
 }
