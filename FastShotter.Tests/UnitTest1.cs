3fa2799cb80524ab9f7df5b9525476d465f0ab92
Krzysztof Krysiak <krysiak86@gmail.com>
2019-08-13 20:48:38 +0200

    Add more unit tests

--- FastShotter.Tests/UnitTest1.cs
+++ FastShotter.Tests/UnitTest1.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace FastShotter.Tests
@@ -16,6 +19,35 @@ namespace FastShotter.Tests
 
             // Assert
             Assert.True(result == 10, "result is true");
+        }
+
+        [Fact]
+        public void Returns_Valid_value()
+        {
+            // Arrange
+            IEnumerable<int> values = new List<int>() {1, 2, 4, 9, 12};
+
+            // Act
+            IEnumerable<int> result = values.Where(x => x < 10).Select(x => x * 2);
+
+            // Asert
+            Assert.Contains(2, result);
+            Assert.Contains(4, result);
+            Assert.Contains(8, result);
+            Assert.Contains(18, result);
+        }
+        
+        [Fact]
+        public void Throws_exception()
+        {
+            // Arrange
+            IEnumerable<int> values = new List<int>() {1, 2, 4, 9, 12};
+
+            // Act
+            IEnumerable<int> result = values.Where(x => x < 10).Select(x => x * 2);
+
+            // Act & Asert
+            Assert.Throws<InvalidOperationException>(() => values.Single(x => x < 5));
         }
 
         private int Execute(int i)
